c6528ab2f9be505cfbcb9dff9c949dee8dbb8497
pvaghani328@gmail.com <pvaghani328@gmail.com>
2022-04-04 12:25:26 +0530

    bug fix of words uppercase

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function converter(sentence){
     let capitalized = []
     let words = sentence.split(" ") //split the sentence into words
     words.forEach(word => { 
-        let capitalizedWord = word.slice(0, 1).toUpperCase()  
-        word.slice(1) //capitalize the first letter of every word
+        let capitalizedWord = word.toUpperCase()   //capitalize the first letter of every word
         capitalized.push(capitalizedWord)         
     })
     let converted = capitalized.join(" ") 
